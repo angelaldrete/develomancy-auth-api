b41588b66dec42b7aa08b822edfc1ed605c9d411
Angel <angel.aldrete96@gmail.com>
2021-04-07 15:03:56 -0700

    changed to mongostore

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 // require('dotenv').config()
 require('./config/db')
+const db = require('./config/db')
 const express = require('express')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const app = express()
 const cors = require('cors')
 const PORT = process.env.PORT || 4000
 const SESSION_SECRET = process.env.SESSION_SECRET
 const UI_URI = process.env.UI_URI
+const STORE_SECRET = process.env.STORE_SECRET
+const DB_CONNECT = process.env.DB_CONNECT
 
 // Middleware
 app.use(cors({ origin: UI_URI, credentials: true }))
@@ -17,7 +21,15 @@ app.use(session({
   saveUninitialized: true,
   cookie: {
     secure: true,
-  }
+  },
+  store: MongoStore.create({
+    mongodb: DB_CONNECT,
+    ttl: 14 * 24 * 60 * 60,
+    autoRemove: 'native',
+    crypto: {
+      secret: STORE_SECRET
+    }
+  })
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
