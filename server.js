2b3bf06a019b6ba4ef72f9ce6bc07e20204c675d
Angel <angel.aldrete96@gmail.com>
2021-04-07 14:48:43 -0700

    change session config

--- server.js
+++ server.js
@@ -1,33 +1,22 @@
 // require('dotenv').config()
 require('./config/db')
-const client = require('./config/cache')
-
 const express = require('express')
 const session = require('express-session')
-const cookieParser = require('cookie-parser')
-const RedisStore = require('connect-redis')(session)
 const app = express()
 const cors = require('cors')
 const PORT = process.env.PORT || 4000
 const SESSION_SECRET = process.env.SESSION_SECRET
 const UI_URI = process.env.UI_URI
-const COOKIE_SECRET = process.env.COOKIE_SECRET
 
 // Middleware
 app.use(cors({ origin: UI_URI, credentials: true }))
-app.enable('trust proxy')
-app.use(cookieParser(COOKIE_SECRET))
+app.set('trust proxy', 1)
 app.use(session({
-  store: new RedisStore({ client: client }),
   secret: SESSION_SECRET,
   resave: false,
-  saveUninitialized: false,
-  proxy: true,
+  saveUninitialized: true,
   cookie: {
     secure: true,
-    httpOnly: true,
-    maxAge: 360000,
-    sameSite: 'lax'
   }
 }))
 app.use(express.json())
