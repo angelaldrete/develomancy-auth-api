7eed4aa6eb4a5bdce7e0ed64c76c67bacd54d316
Angel <angel.aldrete96@gmail.com>
2021-04-07 14:24:17 -0700

    moved cors to top

--- server.js
+++ server.js
@@ -13,41 +13,24 @@ const SESSION_SECRET = process.env.SESSION_SECRET
 const UI_URI = process.env.UI_URI
 
 // Middleware
+app.use(cors({ origin: UI_URI, credentials: true }))
+app.enable('trust proxy')
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.enable('trust proxy')
-// app.use(cors({ origin: UI_URI, credentials: true }))
-// Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
-// Listen on a specific port via the PORT environment variable
-var port = process.env.CA_PORT || 8080;
-
-var cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
-
-var sess = {
+app.use(session({
   // store: new RedisStore({ client: client }),
   secret: SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  // proxy: true,
+  proxy: true,
   cookie: {
     secure: true,
-    // path: '/',
     httpOnly: true,
     maxAge: 360000,
     sameSite: 'lax'
   }
-}
-
-app.use(session(sess))
+}))
 
 // Routes
 const authRoutes = require('./routes/auth')
