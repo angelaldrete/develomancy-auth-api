6d6c6812b71f4c526a6f1e3ef27e0ee539699c61
Angel <angel.aldrete96@gmail.com>
2021-04-07 17:34:16 -0700

    fixed memcache

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const session = require('express-session')
 const cors = require('cors')
 const MemcachedStore = require('connect-memjs')(session)
-const db = require('./config/db')
+require('./config/db')
 
 const PORT = process.env.PORT || 4000
 const SESSION_SECRET = process.env.SESSION_SECRET
@@ -12,16 +12,16 @@ const UI_URI = process.env.UI_URI
 
 // Middleware
 app.use(cors({ origin: UI_URI, credentials: true }))
-app.set('trust proxy', 1)
+// app.set('trust proxy', 1)
 app.use(session({
   secret: SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: {
-    secure: true,
-    maxAge: 14 * 60 * 60 * 24,
-    httpOnly: false,
-  },
+  // cookie: {
+  //   secure: true,
+  //   maxAge: 14 * 60 * 60 * 24,
+  //   httpOnly: false,
+  // },
   store: new MemcachedStore({
     servers: [process.env.MEMCACHIER_SERVERS],
     username: process.env.MEMCACHIER_USERNAME,
