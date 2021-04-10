969d1afc47b49490aa0b2e7c11125d3e790a69ab
Angel <angel.aldrete96@gmail.com>
2021-04-09 20:39:28 -0700

    added cookie

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const SESSION_SECRET = process.env.SESSION_SECRET
 const UI_URI = process.env.UI_URI
 
 // Middleware
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({ origin: UI_URI, credentials: true }))
@@ -22,6 +23,11 @@ app.use(session({
   secret: SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
+  cookie: {
+    sameSite: 'none',
+    secure: true,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  },
   store: new RedisStore({ client })
 }))
 app.use(passport.initialize())
